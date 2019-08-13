d58b2e5075479f7b2dea8dc48305b05728be5efb
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-08-13 11:35:14 +0200

    Add dropdown component

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class ProfileIcon extends React.Component {
-  state = {
-    dropdownOpen: false
-  };
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
 
   render() {
     return (
-      <div class="pa4 tc">
-        <img src="http://tachyons.io/img/logo.jpg" class="br-100 ba h3 w3 dib" alt="avatar" />
-      </div>
+      <React.Fragment>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle caret>Dropdown</DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>View profile</DropdownItem>
+            <DropdownItem>Sign out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <div class="pa4 tc">
+          <img src="http://tachyons.io/img/logo.jpg" class="br-100 ba h3 w3 dib" alt="avatar" />
+        </div>
+      </React.Fragment>
     );
   }
 }
